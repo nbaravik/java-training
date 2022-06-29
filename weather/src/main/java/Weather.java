946e826fe9f37ed7a099298de5f84a0143d8a012
nbaravik <nkachelayeuskaya@gmail.com>
2022-06-29 15:13:08 +0300

    NAT-150 weather bot

---
+++ weather/src/main/java/Weather.java
@@ -0,0 +1,23 @@
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Weather {
+
+    private final Location cityLocation;
+    private final Current currentWeather;
+
+    public Weather(@JsonProperty("location") Location location,
+                   @JsonProperty("current") Current current) {
+        this.cityLocation = location;
+        this.currentWeather = current;
+    }
+
+    public Location getLocation() {
+        return cityLocation;
+    }
+
+    public Current getCurrent() {
+        return currentWeather;
+    }
+}
